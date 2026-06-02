6bd5baadd07c712cc0fc206eec7c0af66f268455
Yusuf Olia <yusufolia21@gmail.com>
2026-06-01 21:55:49 -0400

    Change map tiles to CartoDB Voyager and remove sepia filter to make the ocean blue

--- src/app/components/MigrationMap.tsx
+++ src/app/components/MigrationMap.tsx
@@ -63,8 +63,7 @@ export default function MigrationMap() {
         className="w-full h-full"
       >
         <TileLayer
-          className="leaflet-parchment-layer"
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
 
